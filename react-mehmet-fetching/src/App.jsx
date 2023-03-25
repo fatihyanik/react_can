8d90a38e9b8dd7eacedcb40c8a17d312e9fa2549
fatihyanik <fatihyanik07@gmail.com>
2023-03-25 13:13:43 +0100

    added fetching

---
+++ react-mehmet-fetching/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Axios from './components/Axios'
+import Users from './components/Users'
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <Users /> */}
+      <Axios />
+    </div>
+  )
+}
+
+export default App
---
+++ react-mehmet-fetching/src/components/Axios.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+function Axios() {
+
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        axios('https://jsonplaceholder.typicode.com/users')
+        .then((res) => setUsers(res.data))
+        .catch((e)=>console.log(e))
+        .finally(()=>setIsLoading(false))
+    },[])
+
+    return (
+        <div>
+            <h1>Users</h1>
+            {isLoading && <div>Loading...</div>}
+            {
+                users.map((user, index)=>{
+                    return (
+                        <div key={index}>{user.name}</div>
+                    )
+                })
+            }
+        </div>
+      )
+}
+
+export default Axios
